5b3c9f13d776e56991356927ffbd03f1e7565fd3
Ken Hoff <ken@hoff.tech>
2017-05-14 19:43:54 -0600

    ll -a

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,7 @@ set -x EDITOR atom
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/ken/Downloads/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/ken/Downloads/google-cloud-sdk/path.fish.inc'; else; . '/Users/ken/Downloads/google-cloud-sdk/path.fish.inc'; end; end
+
+function ll --description "List contents of directory using long format"
+    ls -lha $argv
+end
