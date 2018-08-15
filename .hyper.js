406efba10e70a43084e01c8e02b54607414bf085
Ken Hoff <ken@hoff.tech>
2018-08-15 14:51:56 -0600

    hyperlayout

--- .hyper.js
+++ .hyper.js
@@ -104,7 +104,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hypercwd"],
+  plugins: ["hypercwd", "hyperlayout"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
