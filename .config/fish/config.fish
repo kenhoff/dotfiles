b4d290856a9c170b70d6ad55cfef07e3e5a975d5
Ken Hoff <ken@hoff.tech>
2018-08-15 14:56:45 -0600

    pulling some old stuff from fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,4 @@
-# should probably make this work even without go
-
-set -x GOPATH $HOME/dev/go
-set -x PATH $PATH $GOPATH/bin
 set -x EDITOR atom
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/ken/Downloads/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/ken/Downloads/google-cloud-sdk/path.fish.inc'; else; . '/Users/ken/Downloads/google-cloud-sdk/path.fish.inc'; end; end
 
 function ll --description "List contents of directory using long format"
     ls -lha $argv
