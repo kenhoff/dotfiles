ad4f3aa6b48b76d66ed961b16373802e822db29f
Ken Hoff <ken@hoff.tech>
2018-11-06 09:30:47 -0700

    adding gs alias to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,7 @@ set -x EDITOR atom
 function ll --description "List contents of directory using long format"
     ls -lha $argv
 end
+
+function gs --description "git status"
+    git status $argv
+end
