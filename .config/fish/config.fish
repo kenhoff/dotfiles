78a795cf57ca0967f17a2b1191f944536922c431
Ken Hoff <ken@hoff.tech>
2020-01-12 19:54:05 -0700

    adding chrome command line path doop a doo

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,4 +8,9 @@ function gs --description "git status"
     git status $argv
 end
 
+alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+
 thefuck --alias | source
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/ken/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/ken/Downloads/google-cloud-sdk/path.fish.inc'; end
