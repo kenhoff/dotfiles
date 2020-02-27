241129cf0b174a834ad9d9f7666f98f9a0183174
Ken Hoff <ken@hoff.tech>
2020-02-27 13:42:51 -0700

    git commands, removing rvm and google cloud args

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,10 +10,15 @@ function gs --description "git status"
     git status $argv
 end
 
+function gd --description "git diff"
+    git diff $argv
+end
+
+function gc --description "git commit"
+    git commit -m $argv
+end
+
+
 alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
 
 thefuck --alias | source
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/ken/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/ken/Downloads/google-cloud-sdk/path.fish.inc'; end
-rvm default
