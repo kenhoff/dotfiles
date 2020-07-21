63c5e36c34c438536886235ebbb9ce812e160f06
Ken Hoff <ken@hoff.tech>
2020-07-21 08:52:03 -0600

    shitty conda stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 set -x EDITOR atom
 set -gx PATH ~/.hyper_plugins $PATH
-set -g fish_user_paths "/usr/local/opt/mongodb@3.4/bin" $fish_user_paths
+# set -gx PATH /usr/local/anaconda3/bin $PATH  # commented out by conda initialize
 
 function ll --description "List contents of directory using long format"
     ls -lha $argv
@@ -22,3 +22,8 @@ end
 alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
 
 thefuck --alias | source
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /usr/local/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
