3b81b813261d1b601b336a1ec5e84ebfacc4840d
Ken Hoff <ken@hoff.tech>
2020-07-21 07:54:23 -0600

    removing mongo, rvm from fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,5 @@
 set -x EDITOR atom
 set -gx PATH ~/.hyper_plugins $PATH
-set -g fish_user_paths "/usr/local/opt/mongodb@3.4/bin" $fish_user_paths
 
 function ll --description "List contents of directory using long format"
     ls -lha $argv
@@ -16,4 +15,3 @@ thefuck --alias | source
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/ken/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/ken/Downloads/google-cloud-sdk/path.fish.inc'; end
-rvm default
