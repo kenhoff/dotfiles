288e2de815352c8a7c15c749abfb143cdf78629a
Ken Hoff <ken@hoff.tech>
2021-05-12 22:02:08 -0600

    remove conda and gcloud bullshit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,3 @@
-set -gx PATH ~/.hyper_plugins $PATH
-# set -gx PATH /usr/local/anaconda3/bin $PATH  # commented out by conda initialize
 # set -x EDITOR nvim
 set -x EDITOR code
 
@@ -23,15 +21,8 @@ function gc --description "git commit"
     git commit -m $argv
 end
 
-
 alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
 
 thefuck --alias | source
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /usr/local/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/ken/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/ken/Downloads/google-cloud-sdk/path.fish.inc'; end
