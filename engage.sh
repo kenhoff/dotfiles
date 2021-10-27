8538fa1799406a30c233c87862a3b299e9bf196d
Ken Hoff <ken@hoff.tech>
2021-10-27 13:44:29 -0600

    Update brew install command to use sh version

--- engage.sh
+++ engage.sh
@@ -24,7 +24,7 @@ done
 
 
 # install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install node
 brew install neovim
