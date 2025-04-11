6639826ec1103720f78b58dc60de82320350776f
Ken Hoff <ken.hoff@gridx.com>
2025-04-11 09:03:25 -0600

    notes on installing node, setting up fish again

--- macos.sh
+++ macos.sh
@@ -1,14 +1,16 @@
-
-
 # todo - check if brew exists. if not, download and install, including "next steps" bits
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# todo - bit of a race condition here. this is the fish version. zsh wants to install its own version of this, but we need brew installed before we can install fish.
-(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/ken/.profile
+echo >>/Users/ken.hoff/.config/fish/config.fish
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>/Users/ken.hoff/.config/fish/config.fish
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-brew install node
+# todo - bit of a race condition here. this is the fish version. zsh wants to install its own version of this, but we need brew installed before we can install fish.
+# (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/ken.hoff/.profile
+# eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# brew install node # node should be installed by nodenv
 brew install nodenv
 brew install neovim
 brew install fzf
@@ -17,11 +19,11 @@ brew install tmuxinator
 brew install fish
 brew install ripgrep
 brew install thefuck
-brew install exa
-brew install azure-cli
+brew install eza
+brew install --cask dbeaver-community
 
 # connect fish the right way
-echo /usr/local/bin/fish | sudo tee -a /etc/shells
-chsh -s /usr/local/bin/fish
+echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells
+chsh -s /opt/homebrew/bin/fish
 
 # todo - install magnet, notion, brave via cask
